1166e961dcfaaaf89f7b184d2fe0160ccc01d672
bobbybish81 <bobbybish81@hotmail.com>
2022-11-01 10:37:31 +0100

    clock configuration moved to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
+import { DateTime } from 'luxon';
+import { useState } from 'react';
 import { Board } from './components/Board'
+import './App.css';
 
 function App() {
+
+const [date, setDate] = useState(Date.now());
+const [time, setTime] = useState(DateTime.now());
+
+setInterval(() => {
+  setDate(DateTime.now());
+  setTime(DateTime.now());
+}, 1000)
+
   return (
     <div className="main-container">
     <header className='header'>
       <h1 className="title">World Clocks</h1>
     </header>
-    <Board className="board"/>
+    <Board className="board"
+      date={date}
+      time={time}
+    />
   </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,14 @@
-import { DateTime } from 'luxon';
-import { useState } from 'react';
 import { Clock } from './Clock';
+import { DateTime } from 'luxon';
 import './Board.css';
 
-export const Board = () => {
-    
-const [date, setDate] = useState(Date.now());
-const [time, setTime] = useState(DateTime.now());
-
-setInterval(() => {
-  setDate(DateTime.now());
-  setTime(DateTime.now());
-}, 1000)
+export const Board = ({ date, time }) => {
 
 const timezones = ['Europe/London', 'Europe/Paris', 'Europe/Stockholm', 'Europe/Moscow', 'Asia/Tokyo','America/Los_Angeles', 'America/New_York', 'America/Sao_Paulo'];
 const format = {month: 'long', day: 'numeric', year: 'numeric'};
 
 return <div className='board-container'>
-{timezones.map((timezone, index) => 
+{timezones.map(timezone => 
   <Clock 
       date={date.toLocaleString(format)}
       time={time.setZone(timezone).toLocaleString(DateTime.TIME_WITH_SECONDS)}
